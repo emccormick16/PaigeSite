fc37bda37a753c4fa6a3ce5eb1d4250ab9be804e
Ed McCormick <edward.mccormick3@gmail.com>
2023-03-21 22:31:51 -0500

    added sequelize logic

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,9 +1,14 @@
 import db from "./db";
 import User from "./User";
+import Note from "./Note";
 
 const data = {
+  Note,
   User,
   db,
 };
+
+User.hasMany(Note);
+Note.belongsTo(User);
 
 export default data;
