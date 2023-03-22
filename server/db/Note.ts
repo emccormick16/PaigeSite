fa90eaeef8a8f7acf909ba48834d1127011909f1
Ed McCormick <edward.mccormick3@gmail.com>
2023-03-21 22:28:22 -0500

    added note model

---
+++ server/db/Note.ts
@@ -0,0 +1,37 @@
+import db from "./db";
+import Sequelize, {
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+
+const { UUID, UUIDV4, TEXT, NUMBER } = Sequelize;
+
+interface NoteModel
+  extends Model<
+    InferAttributes<NoteModel>,
+    InferCreationAttributes<NoteModel>
+  > {
+  id: CreationOptional<number>;
+  text: string;
+  sessionLength: string;
+}
+
+const Note = db.define<NoteModel>("user", {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+  },
+  text: {
+    type: TEXT,
+    allowNull: false,
+  },
+  sessionLength: {
+    type: NUMBER,
+    allowNull: false,
+  },
+});
+
+export default Note;
